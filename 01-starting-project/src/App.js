dd2a86fb6d953ba8e3422c2ea71aa13d8f7755bb
ranim mouadeb <rmoaudb66@gmail.com>
2024-07-31 10:27:53 +0100

    deploy with firebase

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy , Suspense } from 'react';
+//import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+//import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+
+const BlogPage =lazy( () => import ('./pages/Blog'));
+const PostPage = lazy (() => import('./pages/Post'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children: [
+          { index: true, element: ( <Suspense> <BlogPage /> </Suspense>),
+            loader: () => import ('./pages/Blog') .then ((module) => module.loader())},
+          { path: ':id', 
+            element:(<Suspense><PostPage /></Suspense> ),
+             loader: (meta)=> import ('./pages/Post') .then ((module) => module.loader(meta))
+            
+           },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
